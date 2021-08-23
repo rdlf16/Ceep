24c6ac3a52155d93324ec7384fb9fed78de5af54
Rodolfo C. Sant' ana <60012988+rdlf16@users.noreply.github.com>
2021-08-23 16:08:13 -0300

    Initial commit

---
+++ precache-manifest.ca70c20e05c101641754d5dec252dbbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52e633d90c4455c2e4e4f731ca130b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c27ba476a69d9fa20ffc",
+    "url": "./static/css/main.ec76df02.chunk.css"
+  },
+  {
+    "revision": "408042b4a22a71c23478",
+    "url": "./static/js/2.766e836f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.766e836f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27ba476a69d9fa20ffc",
+    "url": "./static/js/main.f009e2ba.chunk.js"
+  },
+  {
+    "revision": "26c281efe5c8f614022a",
+    "url": "./static/js/runtime-main.57b2e765.js"
+  },
+  {
+    "revision": "56397e007532c05f39537f2a5b538cf1",
+    "url": "./static/media/delete.56397e00.svg"
+  }
+]);
